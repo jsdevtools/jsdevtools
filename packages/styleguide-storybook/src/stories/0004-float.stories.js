588a8050b982fcaf66c59dafcef8e35a098ddee3
Justin Staubach <justin@staubach.us>
2020-02-18 07:50:05 -0500

    docs: add story for float which includes placeholder.
    affects: @jsdt/styleguide-storybook, @jsdt/float

---
+++ packages/styleguide-storybook/src/stories/0004-float.stories.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Float from '@jsdt/float';
+import { withA11y } from '@storybook/addon-a11y';
+
+export default {
+  title: 'Components/Float',
+  component: Float,
+  decorators: [withA11y],
+  parameters: {
+    viewport: { defaultViewport: 'iphone6' },
+  },
+};
+
+export const topLeft = () => <Float placement="topLeft" zIndex={20} margin="10px" />;
+export const topRight = () => <Float placement="topRight" zIndex={20} margin="10px" />;
+export const top = () => <Float placement="top" zIndex={20} margin="10px" />;
+
+topLeft.story = { name: 'Top Left' };
+topRight.story = { name: 'Top Right' };
+top.story = { name: 'Top' };
